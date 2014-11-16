a69ee43529ead37a94cb6ebd3c553388734c9567
Wickerman89 <wojtek.jaromin@gmail.com>
2014-11-16 20:09:37 +0100

    Let's go!

---
+++ Matrix/CoreTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Matrix
+{
+    [TestClass]
+    public class CoreTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
